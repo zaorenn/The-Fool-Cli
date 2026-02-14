69baaf7d69de45a8a286df5a07bf0eaf1cdf08b3
VeryLiu-lab <icey.liu1994@gmail.com>
2026-02-14 21:43:09 +0800

    feat: update dingtalk doc for better ux

--- src/renderer/components/SettingsModal/contents/DingTalkConfigForm.tsx
+++ src/renderer/components/SettingsModal/contents/DingTalkConfigForm.tsx
@@ -56,7 +56,7 @@ interface DingTalkConfigFormProps {
   onStatusChange: (status: IChannelPluginStatus | null) => void;
 }
 
-const DINGTALK_DEV_DOCS_URL = 'https://open.dingtalk.com/';
+const DINGTALK_DEV_DOCS_URL = 'https://github.com/iOfficeAI/AionUi/wiki/DingTalk-Bot-Setup-Guide';
 
 const DingTalkConfigForm: React.FC<DingTalkConfigFormProps> = ({ pluginStatus, modelSelection, onStatusChange }) => {
   const { t } = useTranslation();
