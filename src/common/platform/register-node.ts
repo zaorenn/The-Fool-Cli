2b65dc8e843fa46148ee2ab1a2afb617e39a98ff
zynx <>
2026-04-22 17:22:24 +0800

    chore: remove dead register-node.ts (standalone bootstrap)

--- src/common/platform/register-node.ts
+++
@@ -1,6 +0,0 @@
-// Side-effect module. Import this as the FIRST import in server.ts.
-// It must have no transitive dependencies that call getPlatformServices().
-import { registerPlatformServices } from './index';
-import { NodePlatformServices } from './NodePlatformServices';
-
-registerPlatformServices(new NodePlatformServices());
